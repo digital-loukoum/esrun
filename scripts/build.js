12f7c2284124549631202652e62b8f55293c8cf7
lepzulnag <gindre.matthieu@gmail.com>
2021-12-02 11:44:12 +0100

    📦 Use updated tsc-esm builder

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,17 @@
-import { build } from "@digitak/tsc-esm"
+import { compile, patch } from "@digitak/tsc-esm"
 import { rmSync, chmodSync } from "fs"
 
 console.log("Cleaning library...")
 rmSync("library", { recursive: true, force: true })
 
 console.log("Compiling typescript...")
-await build()
+compile()
+
+console.log("Patching imports...")
+patch([
+	{ find: /^chokidar$/, replacement: null },
+])
 
 console.log("Making binary executable...")
 chmodSync("library/bin.js", 0o775)
+
